3179ba7ea8af0d7e799c7dc70892fd4323b5c1af
PankratovRoman <r.u.pankratov@gmail.com>
2021-03-19 14:59:32 +0300

    update

--- learnin_test/Program.cs
+++ learnin_test/Program.cs
@@ -2,11 +2,38 @@
 
 namespace learnin_test
 {
+    class Pacani
+    {
+        public string name;
+        public virtual void ShoditVTubzik()
+        {
+            Console.WriteLine("{0} пошел покакать!", name);
+        }
+    }
+
+    class Seruni : Pacani
+    {
+        public override void ShoditVTubzik()
+        {
+            Console.WriteLine("{0} - ебучий серун! Опять засрал весь унитаз!", name);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pacani[] kakateli = new Pacani[2];
+            Pacani vitek = new Pacani();
+            vitek.name = "Витек";
+            Seruni misha = new Seruni();
+            misha.name = "Мишаня с пятого этажа";
+            kakateli[0] = vitek;
+            kakateli[1] = misha;
+            foreach (var poci in kakateli)
+            {
+                poci.ShoditVTubzik();
+            }
         }
     }
 }
