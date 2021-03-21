978839b9bd45c691be2c0d4f3060986c09ddf1e3
PankratovRoman <r.u.pankratov@gmail.com>
2021-03-21 21:50:48 +0300

    upd

---
+++ learnin_test/Animal.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace learnin_test
+{
+    abstract class Animal
+    {
+        //абстрактный класс - экземпляр не создается, но создается общий функционал.
+        //abstract - ключевое слово, указывает, что класс абстрактный. Если попытаемся создать экземпляр, то компилятор ругнется.
+        private string _name; //с этим мы ничего не делали, только в образовательных целях
+        public string Name
+        {
+            get
+            {
+                return (_name.Substring(0, 1).ToUpper() + _name.Substring(1).ToLower());
+            }
+            set
+            {
+                _name = value;
+            }
+        } //value - ключевое слово. Означает переданное значение.
+
+        protected readonly int MaxAge; //поля ридонли можно только вытаскивать и всегда необходимо определятиь в конструкторе
+
+
+
+        //
+        public abstract void Move(); //абстрактный метод. Тело метода не пишется. Всем наследникам говорит, что етсь этот метод, но тело определяется на нижних уровнях.
+
+        protected Animal(string name, int maxAge)
+        {
+            Name = name;
+            MaxAge = maxAge;
+        }
+
+
+
+    }
+    class Parrot : Animal
+    {
+        public string Color;
+
+        public Parrot(string name, string color) : base(name, 12)
+        {
+            Color = color;
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine("{0} летает!", Name);
+        }
+        public override string ToString() //ToString есть у каждого класса и вызывается скрыто. Выодит информацию об экземпляре классе.
+                                          //В случае переопределения, возвращает заданное значение.
+        {
+            return $"Я - {Color} попугай и живу до {MaxAge} лет";
+        }
+
+
+    }
+    class Dog : Animal
+    {
+        public string Breed;
+
+        public Dog(string name, string breed) : base(name, 15)
+        {
+            Breed = breed;
+        }
+        public override void Move()
+        {
+            Console.WriteLine("{0} бегает!", Name);
+        }
+
+    }
+}
+
+// задание:
+// сабстринг у стринга. Свойство Name должно возвращать поле name, но исправленное так, чтобы первая буква была большая, а остальные маленькие. Делать в get.
--- learnin_test/Program.cs
+++ learnin_test/Program.cs
@@ -4,37 +4,52 @@ namespace learnin_test
 {
     class Pacani
     {
-        public string name;
+        protected string Name; // публичное поле - с большой буквы
+        protected string LevelOfSmell;
+        public Pacani(string name) : this(name, "Нормально все!")
+        {
+
+        }
+        protected Pacani(string name, string levelOfSmell)
+        {
+            Name = name;
+            LevelOfSmell = levelOfSmell;
+        }
         public virtual void ShoditVTubzik()
         {
-            Console.WriteLine("{0} пошел покакать!", name);
+            Console.WriteLine("{0} пошел покакать! {1}", Name, LevelOfSmell);
         }
     }
 
     class Seruni : Pacani
     {
+        public Seruni(string name) : base(name, "Воняет и грязно!")
+        {
+
+        }
         public override void ShoditVTubzik()
         {
-            Console.WriteLine("{0} - ебучий серун! Опять засрал весь унитаз!", name);
+            Console.WriteLine("{0} - ебучий серун! Опять засрал весь унитаз! {1}", Name, LevelOfSmell);
         }
     }
-
     class Program
     {
         static void Main(string[] args)
         {
             // данный код описывает полиморфизм подтипов на примере жизненной ситуации
-            Pacani[] kakateli = new Pacani[2];
-            Pacani vitek = new Pacani();
-            vitek.name = "Витек";
-            Seruni misha = new Seruni();
-            misha.name = "Мишаня с пятого этажа";
-            kakateli[0] = vitek;
-            kakateli[1] = misha;
-            foreach (var poci in kakateli)
-            {
-                poci.ShoditVTubzik();
-            }
+            //Pacani[] kakateli = new Pacani[2];
+            //kakateli[0] = new Pacani("Витек");
+            //kakateli[1] = new Seruni("Мишаня с пятого этажа");
+            //foreach (var poci in kakateli)
+            //{
+            //    poci.ShoditVTubzik();
+            //}
+
+            Parrot popka = new Parrot("Попка", "желтый");
+            Dog tuzik = new Dog("кнОПкА", "риджбек");
+            Console.WriteLine(popka);
+            tuzik.Move();
+
         }
     }
 }
