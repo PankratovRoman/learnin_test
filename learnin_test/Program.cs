7906395bd13f3430445d72d8af14ddeb2feb9950
r.u.pankratov@gmail.com <r.u.pankratov@gmail.com>
2021-03-23 10:39:04 +0300

    upd

--- learnin_test/Program.cs
+++ learnin_test/Program.cs
@@ -2,53 +2,27 @@
 
 namespace learnin_test
 {
-    class Pacani
-    {
-        protected string Name; // публичное поле - с большой буквы
-        protected string LevelOfSmell;
-        public Pacani(string name) : this(name, "Нормально все!")
-        {
-
-        }
-        protected Pacani(string name, string levelOfSmell)
-        {
-            Name = name;
-            LevelOfSmell = levelOfSmell;
-        }
-        public virtual void ShoditVTubzik()
-        {
-            Console.WriteLine("{0} пошел покакать! {1}", Name, LevelOfSmell);
-        }
-    }
-
-    class Seruni : Pacani
-    {
-        public Seruni(string name) : base(name, "Воняет и грязно!")
-        {
-
-        }
-        public override void ShoditVTubzik()
-        {
-            Console.WriteLine("{0} - ебучий серун! Опять засрал весь унитаз! {1}", Name, LevelOfSmell);
-        }
-    }
+   
     class Program
     {
         static void Main(string[] args)
         {
             // данный код описывает полиморфизм подтипов на примере жизненной ситуации
-            Pacani[] kakateli = new Pacani[2];
-            kakateli[0] = new Pacani("Витек");
-            kakateli[1] = new Seruni("Мишаня с пятого этажа");
-            foreach (var poci in kakateli)
-            {
-                poci.ShoditVTubzik();
-            }
+            //Pacani[] kakateli = new Pacani[2];
+            //kakateli[0] = new Pacani("Витек");
+            //kakateli[1] = new Seruni("Мишаня с пятого этажа");
+            //foreach (var poci in kakateli)
+            //{
+            //    poci.ShoditVTubzik();
+            //}
 
-            Parrot popka = new Parrot("Попка", "желтый");
-            Dog tuzik = new Dog("кНоПка", "риджбек", 1.5f);
-            Console.WriteLine(popka);
-            tuzik.Move();
+            //Parrot popka = new Parrot("Попка", "желтый");
+            //Dog tuzik = new Dog("кНоПка", "риджбек", 1.5f);
+            //Console.WriteLine(popka);
+            //tuzik.Move();
+
+
+
 
         }
     }
---
+++ learnin_test/Seruni.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace learnin_test
+{
+    class Pacani
+    {
+        protected string Name; // публичное поле - с большой буквы
+        protected string LevelOfSmell;
+        public Pacani(string name) : this(name, "Нормально все!")
+        {
+
+        }
+        protected Pacani(string name, string levelOfSmell)
+        {
+            Name = name;
+            LevelOfSmell = levelOfSmell;
+        }
+        public virtual void ShoditVTubzik()
+        {
+            Console.WriteLine("{0} пошел покакать! {1}", Name, LevelOfSmell);
+        }
+    }
+
+    class Seruni : Pacani
+    {
+        public Seruni(string name) : base(name, "Воняет и грязно!")
+        {
+
+        }
+        public override void ShoditVTubzik()
+        {
+            Console.WriteLine("{0} - ебучий серун! Опять засрал весь унитаз! {1}", Name, LevelOfSmell);
+        }
+    }
+}
